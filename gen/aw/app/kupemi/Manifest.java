b6211321ea905c50b4d6b85c01a93175cbe8074d
Andreas Wong <andreaswong770@gmail.com>
2014-03-05 04:46:46 +0800

    Clean up

--- gen/aw/app/kupemi/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package aw.app.kupemi;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
